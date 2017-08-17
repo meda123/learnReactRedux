2a2f4663ea768dd8a130ede133931e803b559ba3
meda123 <mrt62@cornell.edu>
2017-08-17 11:17:27 -0700

    Video list rendering, based on default term 'surfboards'.

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,1 +1,25 @@
-search_bar.js
+import  React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // Term stands for search-term 
+        // We only change state in the constructor func 
+        this.state = { term: ''};
+    }
+
+    render() {
+        return (
+            <div>
+            <input 
+                value={this.state.term}
+                onChange={event => this.setState({term: event.target.value})} />
+
+            </div>
+            );
+    }
+}
+
+export default SearchBar;
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,13 +1,38 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = '';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
+
+
+
 
 // Create a new component. Should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return <div> Hi! </div>; 
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
+            this.setState ({ videos });
+            // abve works when key-val are the same aka {videos: videos}
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+   
 }
+
 
 
 // Render component code 
